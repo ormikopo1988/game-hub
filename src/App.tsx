b761a10e0ae23c95747443d33d0201deb6132dcd
Orestis Meikopoulos <ormeikop@microsoft.com>
2023-04-21 15:58:11 +0300

    Built the color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,9 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem gridArea="aside" bg="gold">
-          Aside
-        </GridItem>
+        <GridItem gridArea="aside">Aside</GridItem>
       </Show>
-      <GridItem gridArea="main" bg="dodgerblue">
-        Main
-      </GridItem>
+      <GridItem gridArea="main">Main</GridItem>
     </Grid>
   );
 }
