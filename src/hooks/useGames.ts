8cd0c0c82849c9233a6e1854f4c0f6530d0a1fcf
Orestis Meikopoulos <ormeikop@microsoft.com>
2023-05-03 15:41:21 +0300

    Build game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-interface Game {
+export interface Game {
   id: number;
   slug: string;
   name: string;
