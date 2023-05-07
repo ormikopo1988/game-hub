cb3bf5af0b4ac62d39fe33e73684666425d44e6e
Orestis Meikopoulos <ormeikop@microsoft.com>
2023-05-07 15:45:50 +0300

    Get platforms and genres with static data at first

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,12 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
-import genreService, { Genre } from "../services/genreService";
+import genreService from "../services/genreService";
 import { CACHE_KEY_GENRES } from "../constants";
+import genres from "../data/genres";
 
 const useGenres = () =>
   useQuery({
     queryKey: CACHE_KEY_GENRES,
     queryFn: genreService.getAll,
-    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours,
+    initialData: genres,
   });
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,12 +1,14 @@
-import platformService, { Platform } from "../services/platformService";
+import platformService from "../services/platformService";
 import { CACHE_KEY_PLATFORMS } from "../constants";
 import { useQuery } from "@tanstack/react-query";
+import platforms from "../data/platforms";
 
 const usePlatforms = () =>
   useQuery({
     queryKey: CACHE_KEY_PLATFORMS,
     queryFn: platformService.getAll,
-    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours,
+    initialData: platforms,
   });
 
 export default usePlatforms;
