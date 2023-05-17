9ee16c880e17f95ed95b314ad35a4a7615f289be
Orestis Meikopoulos <ormeikop@microsoft.com>
2023-05-17 16:08:08 +0300

    Built game attributes component

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,10 +1,14 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
   slug: string;
   name: string;
   released: string;
+  genres: Genre[];
+  publishers: Publisher[];
   background_image: string;
   rating: number;
   rating_top: number;
